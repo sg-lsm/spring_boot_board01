b836795e7b4a3de89a011b1dbb9441c15237af1c
lsm <70528905+sg-lsm@users.noreply.github.com>
2023-07-24 13:37:27 +0900

    http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/lsm/boot_project01/config/CustomServletConfig.java
+++ src/main/java/com/lsm/boot_project01/config/CustomServletConfig.java
@@ -1,4 +1,10 @@
 package com.lsm.boot_project01.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
 public class CustomServletConfig {
+
 }
