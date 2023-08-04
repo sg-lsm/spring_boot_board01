6c0461c13da69f215962e7f75b4086918159d20b
lsm <70528905+sg-lsm@users.noreply.github.com>
2023-08-04 22:47:48 +0900

    reply delete

--- src/main/java/com/lsm/boot_project01/controller/ReplyController.java
+++ src/main/java/com/lsm/boot_project01/controller/ReplyController.java
@@ -1,16 +1,17 @@
 package com.lsm.boot_project01.controller;
 
+import com.lsm.boot_project01.dto.PageRequestDTO;
+import com.lsm.boot_project01.dto.PageResponseDTO;
 import com.lsm.boot_project01.dto.ReplyDTO;
+import com.lsm.boot_project01.service.ReplyService;
 import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -19,20 +20,51 @@ import java.util.Map;
 @RestController
 @RequestMapping("/replies")
 @Log4j2
+@RequiredArgsConstructor // 의존성 주입을 위해서 (final이 붙은 field 중에 초기화되지 않은 모든 field를 arg로 설정해준다) => ReplyController가 ReplyService를 반드시 받도록
 // https://velog.io/@ckdgml1227/Swagger-UI
 public class ReplyController {
+    private final ReplyService replyService;
     @ApiOperation(value = "Replies POST", notes = "POST 방식의 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException{
+
         log.info(replyDTO);
+
 
         if(bindingResult.hasErrors()){
             throw new BindException(bindingResult);
         }
 
-        Map<String, Long> resultMap = new HashMap<>();
-        resultMap.put("rno", 111L);
 
+        Map<String, Long> resultMap = new HashMap<>();
+        Long rno = replyService.register(replyDTO);
+
+        resultMap.put("rno", rno);
+
+        return resultMap;
+    }
+
+    @ApiOperation(value = "Replies of Board", notes="GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value="/list/{bno}")
+    // 호출하는 경로의 값을 직접 파라미터의 변수로 처리해주기 위해 @PathVariable
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO){
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
+        return responseDTO;
+    }
+
+    @ApiOperation(value = "Read Reply", notes = "GET 방식으로 특정 댓글의 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno){
+        ReplyDTO replyDTO = replyService.read(rno);
+        return replyDTO; // 존재않는 번호에 대한 에러처리는 CustomRestAdvice에서
+    }
+
+    @ApiOperation(value = "Delete Reply", notes = "DELETE 방식으로 특정 댓글 삭제")
+    @DeleteMapping("/{rno}")
+    public Map<String, Long> remove(@PathVariable("rno") Long rno){
+        replyService.remove(rno);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
         return resultMap;
     }
 }
--- src/main/java/com/lsm/boot_project01/controller/advice/CustomRestAdvice.java
+++ src/main/java/com/lsm/boot_project01/controller/advice/CustomRestAdvice.java
@@ -2,6 +2,8 @@ package com.lsm.boot_project01.controller.advice;
 // REST 방식의 컨트롤러는 대체적으로 Ajax와 같이 흐름을 직관적으로 알 수 없는 방식으로 서버를 호출하고 결과를 전송하므로 에러가 발생하면 어디에서 어떤 에러가 발생했는지 알기 어렵다. 이런 이유로 @Valid 과정에서 문제가 발생하면 처리할 수 있도록 @RestControllerAdvice를 만들어 처리하려고 한다.
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,7 +20,7 @@ import java.util.Map;
 public class CustomRestAdvice {
     @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
-    public ResponseEntity<Map<String, String>> handlerBinException(BindException e){
+    public ResponseEntity<Map<String, String>> handleBindException(BindException e){
         //log.info(e)
         log.error(e);
 
@@ -30,6 +32,29 @@ public class CustomRestAdvice {
                 errorMap.put(error.getField(), error.getCode());
             });
         }
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    // 존재하지 않는 게시물 번호 (bno)로 검색할 경우 발생하는 에러처리 ->
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String,String>> handleFKException(Exception e){
+        log.error(e);
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("Error Time",""+System.currentTimeMillis());
+        errorMap.put("Message", "constrain failed");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    @ExceptionHandler({NoSuchFieldError.class, EmptyResultDataAccessException.class})
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElements(Exception e){
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time", "" + System.currentTimeMillis());
+        errorMap.put("msg", "No Such Element Exception");
         return ResponseEntity.badRequest().body(errorMap);
     }
 }
