6a495ea07b1a40a46ae1606873346b957b0698aa
lsm <70528905+sg-lsm@users.noreply.github.com>
2023-08-06 15:23:15 +0900

    reply modify/remove event

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -23,3 +23,8 @@ async function modifyReply(replyObj){
     const response = await axios.put(`/replies/${replyObj.rno}`, replyObj);
     return response.data;
 }
+
+async function removeReply(rno){
+    const response = await axios.delete(`/replies/${rno}`);
+    return response.data;
+}
